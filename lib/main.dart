600f442572899a98edef57c9c0ee3f467917bc83
Charly Cardoso <charlycardoso.dev@gmail.com>
2022-01-14 08:35:27 -0600

    cambio de prueba

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    print('prueba');
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Recorridos',
