409a95ed4fddf7358053495ae9cde449d513144f
kvnprog <kevin_mundo@hotmail.com>
2022-01-14 09:56:07 -0600

    segundo

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -34,4 +34,88 @@ class _HomeToursScreenState extends State<HomeToursScreen> {
   String timeValue = '-1';
   bool? isCanceled;
   bool hasBeenCanceled = false;
+
+  @override
+  Widget build(BuildContext context) {
+    if (_opcionSeleccionada != 'Recorrido') {
+      _distance = 80.50;
+    } else {
+      _distance = 120.0;
+    }
+    return MultiProvider(
+        providers: [ChangeNotifierProvider(create: (_) => ProviderListener())],
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Material App',
+          home: Scaffold(
+            resizeToAvoidBottomInset: false,
+            appBar: AppBar(
+              title: const Text('Recorridos'),
+              elevation: 0,
+            ),
+            body: Padding(
+              padding: const EdgeInsets.all(10),
+              child: Column(
+                children: [
+                  _dropDownOptions(),
+                  const SizedBox(height: 30),
+                  if (_opcionSeleccionada == 'Recorrido') _insertPlaces(),
+                  const SizedBox(height: 30),
+                  SizedBox(
+                    height: 375,
+                    child: ListView(
+                      shrinkWrap: true,
+                      children: [
+                        //InteractionMenu(),
+                        for (var menu in interactionMenuArray) menu,
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+            floatingActionButton: ExpandableFab(
+              distance: _distance,
+              children: [
+                //finalizar el recorrido
+                if (_opcionSeleccionada == 'Recorrido')
+                  ActionButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.stop),
+                  ),
+
+                //eliminar un campo de incidencia
+                ActionButton(
+                  icon: const Icon(Icons.delete_forever),
+                  onPressed: () {
+                    setState(() {});
+                    int index = interactionMenuArray.length - 1;
+                    if (index != 0) {
+                      interactionMenuArray.removeAt(index);
+                    }
+                  },
+                ),
+
+                //agregar nuevo campo de crear incidencia
+                ActionButton(
+                  onPressed: () {
+                    setState(() {});
+                    contador += 1;
+
+                    interactionMenuArray.add(InteractionMenu(
+                      index: contador,
+                      usuario: widget.usuario,
+                    ));
+                  }, //_showAction(context, 0),
+                  icon: const Icon(Icons.new_label_sharp),
+                ),
+              ],
+            ),
+          ),
+          theme: ThemeData(
+            scaffoldBackgroundColor: Colors.grey[850],
+            appBarTheme: const AppBarTheme(backgroundColor: Colors.amber),
+          ),
+        ));
+  }
 }
